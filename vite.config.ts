aadf6831d900ec01e68881ddf7bc94c1479ac1cc
imesnyankin <imesnyankin@science-soft.ru>
2025-10-28 13:32:06 +0300

    [FIX] исправили окончательно билд

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/crm-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
